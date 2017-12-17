d267b2db059662da38dd09434ceb1f94db24cecc
Naresh <aluri.naresh540@gmail.com>
2017-12-17 16:46:40 -0500

    Use CTE to correct calculating Steering
    Use CTE to correct calculating Steering
    And Set Acc = 0 when brake is applied

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -23,13 +23,15 @@ class Controller(object):
         self.min_speed = kwargs['min_speed']
         
         #init controllers
-        self.pid = PID(0.4, 0.0, 0.02, self.decel_limit, self.accel_limit)
+        self.pid_linear = PID(0.4, 0.0, 0.02, self.decel_limit, self.accel_limit)
         self.yaw_controller = YawController(self.wheel_base, self.steer_ratio, self.min_speed, self.max_lat_accel, self.max_steer_angle)
         self.steer_filter = LowPassFilter(tau = 2, ts = 1)
         self.throttle_lilter = LowPassFilter(tau = 2, ts = 1)
+        self.pid_steer = PID(0.12, 0.002, 0.1, -self.max_steer_angle, self.max_steer_angle)
 
     def reset(self):
-        self.pid.reset()
+        self.pid_linear.reset()
+        self.pid_steer.reset()
          
     def control(self, *args, **kwargs):
         # get all control params
@@ -43,15 +45,17 @@ class Controller(object):
         
         #get the strring angle update from yaw controller
         steering_update = self.yaw_controller.get_steering(new_linear_vel, new_angular_vel, cur_linear_vel)
-        steering_update = self.steer_filter.filt(steering_update)
+        #steering_update = self.steer_filter.filt(steering_update)
+        steering_update += self.pid_steer.step(cte, duration)#use steer PID to correct the calculated steering update
         
         #get acceleration update from pid 
-        acceleration_update = self.pid.step((new_linear_vel - cur_linear_vel), duration)
+        acceleration_update = self.pid_linear.step((new_linear_vel - cur_linear_vel), duration)
         acceleration_update = self.throttle_lilter.filt(acceleration_update)
         
         #if we were to decelerate, calculate the break update
         if (acceleration_update <= 0.0 and -1.0*acceleration_update >= self.brake_deadband):
              brake_update = -1.0*acceleration_update * (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY) * self.wheel_radius
+             acceleration_update = 0.0
             
         rospy.loginfo("new control info: throttle: %s, brake: %s, steering: %s" %(acceleration_update, brake_update, steering_update))
         return acceleration_update, brake_update, steering_update
